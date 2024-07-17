705e146e3c6c940959705ce894b25622d438a5e9
MarcelinoMajerhua <marcelino.majerhua.dev@gmail.com>
2024-07-17 12:34:23 -0500

    feat: add new columm woner to task table

--- task/createTask.js
+++ task/createTask.js
@@ -2,9 +2,10 @@ const {sendResponse} = require("../functions");
 const {executeQuery} = require("../utils/connection-db");
 module.exports.handler = async (event, context, callback) => {
   const body = JSON.parse(event.body)
+  const owner = event.requestContext.authorizer.claims.email
   const {title, description, status, priority} = body
   await executeQuery(`
-  INSERT INTO tasks (title, description, status, priority)
-  VALUES (?, ?, ?, ?);`, [title, description, status, priority])
+  INSERT INTO tasks (title, description, status, priority, owner)
+  VALUES (?, ?, ?, ?, ?);`, [title, description, status, priority, owner])
   return sendResponse(200, body)
 }
