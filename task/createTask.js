958a69937942fe67b80a4a48f8ea14366c00be30
MarcelinoMajerhua <marcelino.majerhua.dev@gmail.com>
2024-07-17 18:23:37 -0500

    feat: add xray

--- task/createTask.js
+++ task/createTask.js
@@ -1,5 +1,10 @@
 const {sendResponse} = require("../functions");
 const {executeQuery} = require("../utils/connection-db");
+const AWSXRay = require("aws-xray-sdk-core")
+const {LambdaClient, GetAccountSettingsCommand} = require("@aws-sdk/client-lambda")
+
+const lambda = AWSXRay.captureAWSv3Client(new LambdaClient())
+
 module.exports.handler = async (event, context, callback) => {
   const body = JSON.parse(event.body)
   const owner = event.requestContext.authorizer.claims.email
@@ -7,5 +12,10 @@ module.exports.handler = async (event, context, callback) => {
   await executeQuery(`
   INSERT INTO tasks (title, description, status, priority, owner)
   VALUES (?, ?, ?, ?, ?);`, [title, description, status, priority, owner])
+  await getAccountSettings()
   return sendResponse(200, body)
 }
+
+const getAccountSettings = function () {
+  return lambda.send(new GetAccountSettingsCommand());
+};
