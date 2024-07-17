bbf5f0390bba1dd1487cdcbcc57c9960ec24c06b
MarcelinoMajerhua <marcelino.majerhua.dev@gmail.com>
2024-07-17 11:10:09 -0500

    feat: add service to create task

--- task/createTask.js
+++ task/createTask.js
@@ -1,5 +1,10 @@
 const {sendResponse} = require("../functions");
+const {executeQuery} = require("../utils/connection-db");
 module.exports.handler = async (event, context, callback) => {
-  console.log(JSON.stringify(event.body))
-  return sendResponse(200, {message: "Hola mundo desde createTask"})
+  const body = JSON.parse(event.body)
+  const {title, description, status, priority} = body
+  await executeQuery(`
+  INSERT INTO tasks (title, description, status, priority)
+  VALUES (?, ?, ?, ?);`, [title, description, status, priority])
+  return sendResponse(200, body)
 }
