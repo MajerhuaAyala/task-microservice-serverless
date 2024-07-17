be45e8ce6f6789270521b111565410ab18158c13
MarcelinoMajerhua <marcelino.majerhua.dev@gmail.com>
2024-07-17 12:25:41 -0500

    feat: add service to update task

--- task/updateTask.js
+++ task/updateTask.js
@@ -1,5 +1,40 @@
 const {sendResponse} = require("../functions");
+const {executeQuery} = require("../utils/connection-db");
 module.exports.handler = async (event, context, callback) => {
-  console.log(JSON.stringify(event.body))
-  return sendResponse(200, {message: "Hola mundo desde updateTask"})
+  const {id} = event.pathParameters
+  const body = JSON.parse(event.body)
+
+  let queryUpdate = `
+    UPDATE tasks set 
+  `
+  const values = []
+  const updateValues = []
+
+  if (body.title) {
+    updateValues.push(`title = ?`)
+    values.push(body.title)
+  }
+
+  if (body.description) {
+    updateValues.push(`description = ?`)
+    values.push(body.description)
+  }
+
+  if (body.status) {
+    updateValues.push(`status = ?`)
+    values.push(body.status)
+  }
+  if (body.priority) {
+    updateValues.push('priority = ?')
+    values.push(body.priority)
+  }
+
+  if(updateValues.length > 0){
+    queryUpdate += updateValues.join(",")
+    queryUpdate += ` WHERE task_id = ?`
+    values.push(Number(id))
+    await executeQuery(queryUpdate, values)
+  }
+
+  return sendResponse(200, {message: "Update successful"})
 }
