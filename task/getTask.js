0a1173ad070a1d6a7f257515c11309438405ed6b
MarcelinoMajerhua <marcelino.majerhua.dev@gmail.com>
2024-07-17 12:17:12 -0500

    feat: change getTask

--- task/getTask.js
+++ task/getTask.js
@@ -2,7 +2,8 @@ const {sendResponse} = require("../functions");
 const {executeQuery} = require("../utils/connection-db");
 
 module.exports.handler = async (event) => {
-  const {id} = event.queryStringParameters
-  const response = await executeQuery('SELECT * FROM tasks WHERE task_id = ?;', [id])
+  //const {id} = event.queryStringParameters
+  const {id} = event.pathParameters
+  const response = await executeQuery('SELECT * FROM tasks WHERE task_id = ?;', [Number(id)])
   return sendResponse(200, response)
 }
